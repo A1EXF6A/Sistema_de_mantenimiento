bb0ba2983427341c174072572e1bfd38f4086db6
A1EXF6A <100436091+A1EXF6A@users.noreply.github.com>
2024-12-13 02:01:04 -0500

    eliminacion de archivos inecesarios

--- node/database/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('gestionactivos', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3308, // Puerto de MySQL en XAMPP
-});
-
-export default db;
